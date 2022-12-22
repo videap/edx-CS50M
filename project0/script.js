039c37b940ef42a9a4d69f1435d78cd8a61c0210
Vitor Pinto <vitor@nursa.com>
2022-12-21 21:35:39 -0300

    feat(project0): deleteTodo() function

--- project0/script.js
+++ project0/script.js
@@ -3,7 +3,7 @@ const classNames = {
   TODO_ITEM: function () { return `<li class="todo-container" id=${this.TODO_ID}></li>`},
   TODO_CHECKBOX: `<input type="checkbox" class="todo-checkbox">`,
   TODO_TEXT: '<input type="text" class="todo-text">',
-  TODO_DELETE: '<input type="button" class="todo-delete" value="Delete" onClick="deleteTodo()">',
+  TODO_DELETE: function () { return `<input type="button" class="todo-delete" value="Delete" data-id=${this.TODO_ID} onClick="deleteTodo(this)">`},
 }
 
 const list = document.getElementById('todo-list')
@@ -17,11 +17,17 @@ function newTodo() {
   classNames.TODO_ID = id
 
   list.innerHTML += classNames.TODO_ITEM()
-  document.getElementById(id).innerHTML += classNames.TODO_CHECKBOX + classNames.TODO_TEXT + classNames.TODO_DELETE
+  document.getElementById(id).innerHTML += classNames.TODO_CHECKBOX + classNames.TODO_TEXT + classNames.TODO_DELETE()
   countTodo += 1
+  itemCountSpan.innerHTML = countTodo
 }
 
-function deleteTodo(){
-  
+function deleteTodo(element){
+  // todoItemId = element.parentElement.id
+  // document.getElementById(todoItemId).remove()
+  // console.log(todoItemId)
+  document.getElementById(element.getAttribute('data-id')).remove()
+  countTodo -= 1
+  itemCountSpan.innerHTML = countTodo
 }
 
