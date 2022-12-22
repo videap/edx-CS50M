0d6b92b7beee846863575738a8273bbd1a87bea0
Vitor Pinto <vitor@nursa.com>
2022-12-21 22:38:22 -0300

    fix(project0): fix persistent text, count to zero issues

--- project0/script.js
+++ project0/script.js
@@ -2,18 +2,18 @@ const classNames = {
   TODO_ID: 0,
   TODO_ITEM: function () { return `<li class="todo-container" id=${this.TODO_ID}></li>`},
   TODO_CHECKBOX: `<input type="checkbox" class="todo-checkbox" onClick="checkTodos()">`,
-  TODO_TEXT: '<input type="text" class="todo-text">',
+  TODO_TEXT: '<input type="text" class="todo-text" onChange="editTodo(event)">',
   TODO_DELETE: function () { return `<input type="button" class="todo-delete" value="Delete" data-id=${this.TODO_ID} onClick="deleteTodo(this)">`},
 }
 
 const list = document.getElementById('todo-list')
 const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
-let countTodo = 0
+let countTodo = 0, id = 0
 let countUnchecked = 0
 
 function newTodo() {
-  const id = countTodo + 1
+  id += 1
   classNames.TODO_ID = id
 
   list.innerHTML += classNames.TODO_ITEM()
@@ -33,11 +33,19 @@ function deleteTodo(element){
 
 function checkTodos() {
   countUnchecked = 0
-  document.querySelectorAll(".todo-checkbox").forEach(element => {
+  checkboxes = document.querySelectorAll(".todo-checkbox")
+  if (checkboxes.length > 0){
+    checkboxes.forEach(element => {
+      if (element.checked === false){
+        countUnchecked += 1
+      }
+      uncheckedCountSpan.innerHTML = countUnchecked
+    });
+  } else {
+    uncheckedCountSpan.innerHTML = 0
+  }
+}
 
-    if (element.checked === false){
-      countUnchecked += 1
-    }
-    uncheckedCountSpan.innerHTML = countUnchecked
-  });
+function editTodo(event){
+  event.target.setAttribute("value", event.target.value)
 }
