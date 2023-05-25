8863e02dde402fd6902641671071bc2768295d2b
Vitor Pinto <vitor@nursa.com>
2023-05-25 17:27:12 -0300

    chore: updated app to react component instead of function

--- project1/pomodoro/App.js
+++ project1/pomodoro/App.js
@@ -1,25 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import { Platform } from 'react-native';
-import React, {useState} from 'react';
+import React, {Component} from 'react';
+import settings from './settings';
 
-export default function App() {
+export class App extends Component {
+  constructor(props) {
+    super(props);
 
-const [ counter, setCounter ] = useState(0)
+    this.state = {
+      periodSecs: 0,
+      countdownSecs: 0,
+      countPomodoros: 0
+    };
+  }
 
-const increment = () => setCounter(counter+1)
+  startPomodoro() {
+    console.log("Your pomodoro has started!")
+  };
 
-  return (
-    <View style={styles.container}>
-      <Text>Este é o meu primeiro app para {Platform.OS}!!</Text>
-       <Text>Aproveite...</Text>
-      <View style={styles.button}>
-        <Button title={`Count is ${counter}`} onPress={() => {increment()}}></Button>
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>This is the pomodoro app for {Platform.OS}!!</Text>
+        <View style={styles.button}>
+          <Button title={`START POMODORO`} onPress={startPomodoro()}></Button>
+        </View>
+        <View>
+          <Text on>
+            Countdown: {this.state.countdownSecs}
+          </Text>
+        </View>
+        <StatusBar style="auto" />
       </View>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    );
+  };
+};
 
 const styles = StyleSheet.create({
   container: {
