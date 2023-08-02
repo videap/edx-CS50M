3d3cec1a4fe740ae945c34beaa599b24658a4b4b
Vitor Pinto <vitor@nursa.com>
2023-08-02 19:42:10 -0300

    feat(project2): new sniffflix app

---
+++ project2/sniffflix/App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+
+  console.log('running')
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>sniFFFlix!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 48
+  }
+});
